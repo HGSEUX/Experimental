d28427da62e41e485fc52a3b190000c557e12cb6
HGSEUX <ezmilragna2@gmail.com>
2026-09-22 19:21:11 +0800

    Update fatigue.lua

--- data/fatigue.lua
+++ data/fatigue.lua
@@ -11,7 +11,7 @@ d.normal = {
     { time = 7,   event = function() GAME.boardDim = { .8, .8, .8 } end },
     { time = 8,   event = function() GAME.boardDim = { .3, .3, .3 } end },
     { time = 9,   event = function() GAME.boardDim = { .1, .1, .1 } end },
-    { time = 10,   event = { 'dmgTimerMul', -.1, 'animDuration', 1 },                   text = "FATIGUE SETS IN_",          desc = "TimerSpeed++" },
+    { time = 10,   event = { 'dmgTimerMul', -.9, 'animDuration', 1 },                   text = "FATIGUE SETS IN_",          desc = "TimerSpeed++" },
     { time = 0,     event = function() GAME.setFatigueColor('norm') end },
     { time = 301,   event = function() GAME.boardDim = { 1, 1, 1 } end },
 
