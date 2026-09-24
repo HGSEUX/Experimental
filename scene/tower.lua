51b35188c785c9ae9b28af6b343fd1b7a2732b99
HGSEUX <ezmilragna2@gmail.com>
2026-09-24 21:40:03 +0800

    uIN…

--- scene/tower.lua
+++ scene/tower.lua
@@ -1396,7 +1396,7 @@ function scene.overDraw()
             if M.IN == 2 then
                 local k = 1 / i
                 a = clamp(
-                    a * (1 - GAME.questTime * (2 * GAME.floor + 1) * k),
+                    a * (1 - GAME.questTime * (4.95 * GAME.floor + 10) * k),
                     GAME.faultWrong and not URM and i * .26 or 0, 1
                 )
             end
